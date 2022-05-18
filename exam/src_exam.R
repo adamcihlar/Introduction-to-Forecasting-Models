4a9ad0a154e8a21b3998c1e6197f480133357a78
adamcihlar <adam.cihlar3@gmail.com>
2022-05-18 14:11:16 +0200

    Plot timeseries

--- exam/src_exam.R
+++ exam/src_exam.R
@@ -2,14 +2,31 @@
 # get ready
 rm(list = ls())
 dev.off()
-setwd("C:/Users/Adam Cihlár/Desktop/materiály/ForecastingModels/exam")
+setwd("C:/Users/Adam Cihlár/Desktop/materiály/ForecastingModels/assignments/exam/")
 
 # libraries
 library(tidyverse)
 library(readxl)
+library(gridExtra)
 
+# constants
+n_vars <- 10
 
 # functions
 
 
 # load and preprocess data
+dataset <- read_excel('data/112839377-Homeexam OKA2014 - 2022 Sjur Westgaard_112853997_1652444012642.xlsx')
+dataset['date'] <- as.Date(dataset$dateid01)
+
+# plot timeseries
+ts_plots <- map(
+    1:n_vars,
+    ~ ggplot(data = dataset, mapping = aes_string(x='date', y=colnames(dataset[.+1]))) + 
+        geom_line(color = 'steelblue') +
+        theme_bw() +
+        scale_x_date(name = element_blank(), date_minor_breaks = "5 year")
+        #scale_y_continuous(name = "Value")
+    )
+grid.arrange(grobs = ts_plots, ncol = 2)
+
