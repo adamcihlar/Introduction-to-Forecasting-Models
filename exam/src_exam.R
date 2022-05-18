f0b1b7c2ff635f3c911b5d2f2f506d6998fc97ad
adamcihlar <adam.cihlar3@gmail.com>
2022-05-18 13:43:03 +0200

    Add exam folder and src file

---
+++ exam/src_exam.R
@@ -0,0 +1,15 @@
+
+# get ready
+rm(list = ls())
+dev.off()
+setwd("C:/Users/Adam Cihlár/Desktop/materiály/ForecastingModels/exam")
+
+# libraries
+library(tidyverse)
+library(readxl)
+
+
+# functions
+
+
+# load and preprocess data
